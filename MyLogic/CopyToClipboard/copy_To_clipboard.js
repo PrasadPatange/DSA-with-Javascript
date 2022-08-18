5ecb6967872ba766b37602541d669fc89dd18de4
Prasad <prasadpatange2603@gmail.com>
2022-08-18 15:28:16 +0530

    first commit

---
+++ MyLogic/CopyToClipboard/copy_To_clipboard.js
@@ -0,0 +1,22 @@
+window.onload=function(){
+    
+var toCopy = document.getElementById('to-copy');
+btnCopy = document.getElementById('copy');
+
+paste = document.getElementById('cleared');
+    btnCopy.addEventListener('click',function(){
+        toCopy.select();
+        paste.value = '';
+        if( document.execCommand('copy')){
+            btnCopy.classList.add('copied');
+            paste.focus();
+            var temp = setInterval( function(){
+                btnCopy.classList.remove('copied');
+                clearInterval(temp);
+            }, 600);
+        }else{
+            console.log('document.execCommand Went Wrong......')
+        }
+        return false;
+    });
+}
