d67a36b5db7d7c28dd9cdfc7e9bc24adc494746a
Prasad <prasadpatange2603@gmail.com>
2022-11-02 10:39:55 +0530

    imp_Topic.txt

---
+++ Javascript/Js_Imp_Topic/Closures.js
@@ -0,0 +1,49 @@
+/*
+
+Closure : 
+- A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment).
+In other words, a closure gives you access to an outer function's scope from an inner function. 
+In JavaScript, closures are created every time a function is created, at function creation time.
+
+- A closure is the combination of a function and the lexical environment within which that function was declared. 
+
+*/
+console.log("*** Closure :  ***");
+console.log("*** lexical Scoping :  ***");
+
+function init() {
+    var name = 'Mozilla'; // name is a local variable created by init
+    function displayName() {
+        // displayName() is the inner function, a closure
+        console.log(name); // use variable declared in the parent function
+    }
+    displayName();
+}
+init();
+
+/*
+init() creates a local variable called name and a function called displayName().
+The displayName() function is an inner function that is defined inside init() and is available only within the body of the init() function. 
+Note that the displayName() function has no local variables of its own. However, since inner functions have access to the variables of outer functions,
+displayName() can access the variable name declared in the parent function, init().
+*/
+ 
+console.log("*** Closure :  ***");
+
+function makeFunc() {
+    const name = 'Prasad';
+    function displayName() {
+      console.log(name);
+    }
+    return displayName;
+  }
+  
+  const myFunc = makeFunc();
+  myFunc();
+
+/*
+The reason is that functions in JavaScript form closures.
+A closure is the combination of a function and the lexical environment within which that function was declared.
+This environment consists of any local variables that were in-scope at the time the closure was created. In this case, myFunc is a reference to the instance of the function displayName that is created when makeFunc is run. The instance of displayName maintains a reference to its lexical environment, within which the variable name exists. For this reason, when myFunc is invoked,
+ the variable name remains available for use, and "Mozilla" is passed to console.log.
+*/
--- Javascript/Js_Imp_Topic/Hoisting.js
+++ Javascript/Js_Imp_Topic/Hoisting.js
@@ -16,3 +16,35 @@ console.log(b);
 
 // Temporal deadzone : Temporal deadzone is a time . since when this let variable are hoisted 
                      // till it's initialize some value between that time is known as "Temporal Deadzone".
+
+                     
+
+/*
+Temporal Dead Zone : 
+Temporal Dead Zone is the period of time during which the let and const declarations cannot be accessed.
+Temporal Dead Zone starts when the code execution enters the block which contains the let or const declaration and continues until the declaration has executed.  
+*/
+
+
+// Example :
+  
+//  let and const :
+ 
+// carName = 'TATA';
+// console.log(carName);
+// let carName;     // declared  
+// Error : Uncaught ReferenceError: Cannot access 'carName' before initialization
+
+try{
+carName = 'TATA';
+let carName;     // declared  
+}
+catch(e){
+console.log(e)
+}
+
+
+// bikeName = 'Duke';
+// const bikeName; 
+// Error :  Uncaught SyntaxError: Missing initializer in const declaration
+
